d34910a230337bbed1c205741546ebedae5e2d8b
Lauren Riddle <lari7132@gmail.com>
2019-10-16 11:51:13 -0500

    created event listener that puts the content of the textbox into the article fields

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 console.log("THIS IS WORKING!!!")
+
+
+let article1El = document.querySelector("#article1")
+let article2El = document.querySelector("#article2")
+let fieldEl = document.getElementById("message")
+
+fieldEl.addEventListener("keyup", event => {
+  
+  article1El.textContent = event.target.value
+  article2El.textContent = event.target.value
+
+})
